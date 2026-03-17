cf9bd4fefa6c2182a2355dbbe120f020d3c57b0e
praveen <praveendhanapal7@gmail.com>
2026-03-17 23:12:07 +0530

    Changes made

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -27,7 +27,7 @@ function Dashboard() {
           secretKey: user.secretKey,
         };
 
-        const response = await fetch("http://localhost:8010/get/leads/all", {
+        const response = await fetch("/get/leads/all", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -79,7 +79,7 @@ function Dashboard() {
 
     //POSTING leads from backend
     try {
-      await fetch("http://localhost:8010/add/leads", {
+      await fetch("https://forwardbackendserver-production.up.railway.app/add/leads", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -105,7 +105,7 @@ function Dashboard() {
     if (user.secretKey == "forward@2025") {
       async function loadData() {
         try {
-          const response = await fetch("http://localhost:8010/get/all/brands", {
+          const response = await fetch("https://forwardbackendserver-production.up.railway.app/get/all/brands", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
